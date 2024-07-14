2cedc079b6269ccef108a34b9564f16364fcad27
Die Maus <ausdiemouse@gmail.com>
2024-07-14 10:47:00 +0530

    stop_all_containers.sh: added comments about nodejs. see inline comments about setting up as a service and starting the app when the container starts. Graceful shutdown of all containers.

---
+++ stop_all_containers.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+lxc-ls --fancy
+
+#RUNNING_CONTAINERS="$(lxc-ls)"
+RUNNING_CONTAINERS="$(lxc-ls --running)"
+
+#stop services on containers
+lxc-attach -n gitea -- systemctl stop gitea
+lxc-attach -n gitea -- systemctl status gitea
+#lxc-attach -n nodejs -- systemctl status nodejs	#app.js has not been setup to run auto. node is not autostarted. could set it up as a service. and revisit this line.
+
+
+sleep 15
+
+echo "check graceful shutdown of services for other containers"
+
+
+for container in $RUNNING_CONTAINERS; do
+  echo "Stopping $container..."
+  lxc-stop --name $container
+done
+
+lxc-ls --fancy
+
