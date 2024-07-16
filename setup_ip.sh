4e293e6bf89955845d39ea13a340bfa70e3ce59a
Die Maus <ausdiemouse@gmail.com>
2024-07-16 23:32:27 +0530

    setup_ip.sh: added the necessary comments

--- setup_ip.sh
+++ setup_ip.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+
 #iptables --flush
 #iptables --list
 #iptables -L
 
+#following line populates $EXTIP with the host ip automatically 
 EXTIP=$(ip a | grep enp | grep inet | awk '{print $2}'| sed 's/\/.*//')
 
+#this is alternative to the previous line
 #EXTIP=$(ip a | grep -A2 "enp3s0" | grep "inet " | awk '{print $2}' | cut -d/ -f1)
 
+#this was the legacy manual update of the host ip
 #EXTIP=192.168.1.183
 
+#The following similar lines map the containers ip and ports to the host ip and port of choice for external access over the network
 iptables -t nat -I PREROUTING -i enp3s0 -p TCP -d $EXTIP --dport 8001 -j DNAT --to-destination 10.0.3.119:80
 iptables -t nat -I PREROUTING -i enp3s0 -p TCP -d $EXTIP --dport 8002 -j DNAT --to-destination 10.0.3.56:80
 iptables -t nat -I PREROUTING -i enp3s0 -p TCP -d $EXTIP --dport 8003 -j DNAT --to-destination 10.0.3.178:80
@@ -17,6 +22,7 @@ iptables -t nat -I PREROUTING -i enp3s0 -p TCP -d $EXTIP --dport 8004 -j DNAT --
 iptables -t nat -I PREROUTING -i enp3s0 -p TCP -d $EXTIP --dport 3000 -j DNAT --to-destination 10.0.3.55:3000
 iptables -t nat -I PREROUTING -i enp3s0 -p TCP -d $EXTIP --dport 8005 -j DNAT --to-destination 10.0.3.14:8080
 
+#Reference of containers
 #NAME          STATE   AUTOSTART GROUPS IPV4       IPV6 UNPRIVILEGED 
 #lamp          RUNNING 1         -      10.0.3.119 -    false        
 #lamp_copy     RUNNING 1         -      10.0.3.65  -    false        
