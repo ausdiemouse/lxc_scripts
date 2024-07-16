9cac9e2822cea33933c614886a0d008d8fefdb62
Die Maus <ausdiemouse@gmail.com>
2024-07-16 09:52:50 +0530

    update_all_containers.sh: updated script to capture all running containers as lxc-attach doesnt connect to stopped containers. also added an line to read in an external script for execution in the container - generic_cleanup.sh - script does the update and upgrade and autoremove/clean

--- update_all_containers.sh
+++ update_all_containers.sh
@@ -3,14 +3,16 @@
 lxc-ls --fancy
 echo " "
 
-RUNNING_CONTAINERS="$(lxc-ls)"
+#RUNNING_CONTAINERS="$(lxc-ls)"
+RUNNING_CONTAINERS="$(lxc-ls --running)"
 
 
 for container in $RUNNING_CONTAINERS; do
   echo "Updating $container..."
 
-  lxc-attach --name $container -- apt update
-  lxc-attach --name $container -- apt -y upgrade
+#  lxc-attach --name $container -- apt update
+#  lxc-attach --name $container -- apt -y upgrade
+  lxc-attach --name $container -- < generic_cleanup.sh
 
   echo "Updated $container."
   echo "-------------------"
