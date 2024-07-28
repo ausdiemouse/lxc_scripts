321be4e6625d5c0cf8a7e005c217d8c279947c15
ausdiemouse <ausdiemouse@gmail.com>
2024-07-28 08:02:29 +0530

    setup_ip.sh: n/w interface capture more effient and compact -AI, spawns fewer processes

--- setup_ip.sh
+++ setup_ip.sh
@@ -18,9 +18,11 @@ EXTIP=$(ip a | grep -E "enp|wlan" | grep inet | awk '{print $2; exit}'| sed 's/\
 # n/w interface capture starts ---------------
 
 #reads in either the ethernet or wifi interface whichever is first. or whichever is up 
-interface=$(ip a | grep -E "enp|wlan" | grep "state UP"| awk '{print $2; exit}'| sed 's/:.*//')
+#interface=$(ip a | grep -E "enp|wlan" | grep "state UP"| awk '{print $2; exit}'| sed 's/:.*//')
+#improved spawns lesser processes -in AI we trust ... tested works. query:"more efficient and compact"
+interface=$(ip -o link show | awk -F': ' '/wlan|enp/ && /state UP/{print $2; exit}')
 
-# n/w interface capture ends x-----------------x
+# n/w interface capture ends x-----ls------------xd
 
 
 #this was the legacy manual update of the host ip
@@ -28,7 +30,6 @@ interface=$(ip a | grep -E "enp|wlan" | grep "state UP"| awk '{print $2; exit}'|
 
 echo "interface: $interface"
 echo "ip: $EXTIP"
-
 
 #The following similar lines map the containers ip and ports to the host ip and port of choice for external access over the network
 #for ethernet interface and corresponding ip 	#need to re-write for ip block redundancy
@@ -38,7 +39,6 @@ iptables -t nat -I PREROUTING -i $interface -p TCP -d $EXTIP --dport 8003 -j DNA
 iptables -t nat -I PREROUTING -i $interface -p TCP -d $EXTIP --dport 8004 -j DNAT --to-destination 10.0.3.35:8080
 iptables -t nat -I PREROUTING -i $interface -p TCP -d $EXTIP --dport 3000 -j DNAT --to-destination 10.0.3.55:3000
 iptables -t nat -I PREROUTING -i $interface -p TCP -d $EXTIP --dport 8005 -j DNAT --to-destination 10.0.3.14:8080
-
 
 
 #Reference of containers
